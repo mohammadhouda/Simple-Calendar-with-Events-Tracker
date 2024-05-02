966b56f9ac2513e2e281772864ff498fdd64fa49
mo <mo@gmail.com>
2024-05-02 22:42:32 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,72 @@
+date = new Date();
+
+let months = [
+  "january",
+  "february",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+let prevMonth = document.querySelectorAll(".icons span");
+
+let days = document.querySelector(".days-list");
+let headDate = document.querySelector(".date");
+let currentMonth = date.getMonth();
+let currentYear = date.getFullYear();
+renderCalendar();
+
+function renderCalendar() {
+  let firstDayofTheMonth = new Date(currentYear, currentMonth, 1).getDay();
+  let lastDateofTheMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+  let lastDateofTheLastMonth = new Date(currentYear, currentMonth, 0).getDate();
+  let lastDayOfTheMonth = new Date(
+    currentYear,
+    currentMonth,
+    lastDateofTheMonth
+  ).getDay();
+
+  headDate.innerText = `${months[currentMonth]} ${currentYear}`;
+  let listDays = "";
+
+  for (i = firstDayofTheMonth; i > 0; i--) {
+    listDays += `<li class="inactive">${lastDateofTheLastMonth - i + 1}</li> `;
+  }
+
+  for (i = 1; i <= lastDateofTheMonth; i++) {
+    let isToday =
+      i === date.getDate() &&
+      currentMonth === date.getMonth() &&
+      currentYear === date.getFullYear()
+        ? "active"
+        : "";
+
+    listDays += `<li class="${isToday}">${i}</li> `;
+  }
+
+  for (i = lastDayOfTheMonth; i < 6; i++) {
+    listDays += `<li class="inactive">${i - lastDayOfTheMonth + 1}</li> `;
+  }
+
+  days.innerHTML = listDays;
+}
+
+prevMonth.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    currentMonth = icon.id === "prev" ? currentMonth - 1 : currentMonth + 1;
+    renderCalendar();
+
+    if (currentMonth < 0 || currentMonth > 11) {
+      date = new Date(currentYear, currentMonth);
+      currentYear = date.getFullYear();
+      currentMonth = date.getMonth();
+    }
+  });
+});
